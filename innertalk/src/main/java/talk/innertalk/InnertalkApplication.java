54d7a46dc15ef57cd2164108a1f6ea7ddc8db640
crane0731 <dlwnsgkr8318@naver.com>
2025-04-07 14:04:44 +0900

    초기 커밋: 프로젝트 세팅 완료

---
+++ innertalk/src/main/java/talk/innertalk/InnertalkApplication.java
@@ -0,0 +1,13 @@
+package talk.innertalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnertalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnertalkApplication.class, args);
+	}
+
+}
---
+++ innertalk/src/test/java/talk/innertalk/InnertalkApplicationTests.java
@@ -0,0 +1,13 @@
+package talk.innertalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnertalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
